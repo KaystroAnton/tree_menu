1329de6207698d643c5b5f18561899b051ecd0a8
KaystroAnton <anton.kaystro@mail.ru>
2025-08-14 16:19:40 +0300

    final

--- menu/views.py
+++ menu/views.py
@@ -9,14 +9,14 @@ def show_menu(request):
     return render(request, "menu/menu.html")
 
 def creat_table(model):
-    menu = model.objects.create(name="menu",url = 'http://127.0.0.1:8000/')
-    menu2 = model.objects.create(name="menu2", url='http://127.0.0.1:8000/000')
-    menu3 = model.objects.create(name="menu3", url='http://127.0.0.1:8000/000')
-    main2 = model.objects.create(name="Главная2", parent=menu ,url='http://127.0.0.1:8000/000')
-    main1 = model.objects.create(name="Главная",parent=menu ,url = 'http://127.0.0.1:8000/000')
-    model.objects.create(name="Магазин", parent=main1, url = 'SubLevel 2.1')
-    model.objects.create(name="О нас", parent=main1,url = 'SubLevel 2.2')
-    model.objects.create(name="menu5",url = 'http://127.0.0.1:8000/')
+    menu = model.objects.create(name="menu",url = 'http://127.0.0.1:8000/00')
+    menu2 = model.objects.create(name="menu2", url='menu2')
+    menu3 = model.objects.create(name="menu3", url='menu3')
+    main2 = model.objects.create(name="Главная2", parent=menu ,url='main2')
+    main1 = model.objects.create(name="Главная",parent=menu ,url = 'http://127.0.0.1:8000/')
+    model.objects.create(name="Магазин", parent=main1, url = 'Магазин')
+    model.objects.create(name="О нас", parent=main1,url = 'О_нас')
+    model.objects.create(name="menu5",url = 'menu5')
 def del_all(model):
     for obj in model.objects.all():
         obj.delete()
